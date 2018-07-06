3e1c96bc10749da11bb0a65c995d356d539fdeb4
SobhaPothuganti <40598670+SobhaPothuganti@users.noreply.github.com>
2018-07-06 13:58:31 +0530

    Update DeleteNode.java

--- week2/day1/DeleteNode.java
+++ week2/day1/DeleteNode.java
@@ -1,1 +1,19 @@
+public class DeleteNode {
+    public static class LinkedListNode {
+
+        public int value;
+        public LinkedListNode next;
+
+        public LinkedListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void deleteNode(LinkedListNode nodeToDelete) {
+        if(nodeToDelete.next == null) throw new IllegalArgumentException("");
+        else {
+            nodeToDelete.value=nodeToDelete.next.value;
+            nodeToDelete.next=nodeToDelete.next.next;
+        }
+    }
 
