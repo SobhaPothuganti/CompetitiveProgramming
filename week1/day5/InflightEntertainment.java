4203abaf22919db54266e25eab3db84454f21072
SobhaPothuganti <40598670+SobhaPothuganti@users.noreply.github.com>
2018-07-06 11:22:43 +0530

    Update InflightEntertainment.java

--- week1/day5/InflightEntertainment.java
+++ week1/day5/InflightEntertainment.java
@@ -1,18 +1,10 @@
 public class InflightEntertainment {
 
     public static boolean canTwoMoviesFillFlight(int[] movieLengths, int flightLength) {
-
-        // determine if two movies add up to the flight length
         int n=movieLengths.length;
         HashMap<Integer,Integer> hm=new HashMap();
         //int i=0,j=n-1;
         if(n<=1)return false;
-        /*Arrays.sort(movieLengths);
-        while(i<j){
-            if(movieLengths[i]+movieLengths[j]==flightLength) return true;
-            else if(movieLengths[i]+movieLengths[j]>flightLength) j--;
-            else i++;
-        }*/
         for(int i=0;i<n;i++){
             int k=flightLength-movieLengths[i];
             if(hm.containsKey(k)) return true;
