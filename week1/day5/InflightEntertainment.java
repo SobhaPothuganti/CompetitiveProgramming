5f6f5ac179f032ac204c6d4668279730d3a9f153
SobhaPothuganti <40598670+SobhaPothuganti@users.noreply.github.com>
2018-07-06 11:21:03 +0530

    Update InflightEntertainment.java

--- week1/day5/InflightEntertainment.java
+++ week1/day5/InflightEntertainment.java
@@ -1,16 +1,1 @@
-public class Fibonacci {
-    static int a[];
 
-    public static int fib(int n) {
-
-    if(n<0) throw new IllegalArgumentException("hello");
-        // compute the nth Fibonacci number
-        a=new int[n+1];
-        int p=fibonacci(n);
-        return p;
-    }
-    public static int fibonacci(int n){
-        if(n==0 || n==1) return n;
-        else if(a[n]!=0) return a[n];
-        else return a[n]=fib(n-1)+fib(n-2);
-    }
