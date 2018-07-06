92118477d597581451f5acd7d7c507a346585dfd
SobhaPothuganti <40598670+SobhaPothuganti@users.noreply.github.com>
2018-07-06 11:44:11 +0530

    Update QueueUsing2Stacks.java

--- week1/day6/QueueUsing2Stacks.java
+++ week1/day6/QueueUsing2Stacks.java
@@ -1,1 +1,30 @@
+public class QueuewithTwoStacks {
 
+    // fill in the definitions for enqueue() and dequeue() 
+
+    public static class QueueTwoStacks {
+
+        private Stack<Integer> inStack = new Stack<>();
+        private Stack<Integer> outStack = new Stack<>();
+        
+        public void enqueue(int item) {
+            inStack.push(item);
+        }
+
+        public int dequeue() {
+            if(inStack.empty() && outStack.empty()) throw new IllegalArgumentException("");
+            else if(outStack.empty()){
+                while(!inStack.empty()){
+                    outStack.push(inStack.pop());
+                }
+                int k=outStack.pop();
+                /*while(!outStack.empty()){
+                    inStack.push(outStack.pop());
+                }*/
+                return k;
+            }
+            else{
+                return outStack.pop();
+            }
+        }
+    }
