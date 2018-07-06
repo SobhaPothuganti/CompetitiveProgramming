98e6b261de774ada988c292a9d2f6774c2bc2632
SobhaPothuganti <40598670+SobhaPothuganti@users.noreply.github.com>
2018-07-06 11:18:36 +0530

    Update OrderedSet.java

--- week1/day4/OrderedSet.java
+++ week1/day4/OrderedSet.java
@@ -1,1 +1,28 @@
+public class FindinOrderedSet {
 
+    public static boolean contains(int[] a, int n) {
+
+        // check if an integer is present in the array
+       int result = binarySearch(a
+       ,0,a.length-1,n);
+        if (result == -1)
+            return false;
+        else
+            return true; 
+
+    } 
+    static int binarySearch(int arr[], int l, int r, int x)
+    {
+        if (r>=l)
+        {
+            int mid = l + (r - l)/2;
+            if (arr[mid] == x)
+               return mid;
+ 
+            if (arr[mid] > x)
+               return binarySearch(arr, l, mid-1, x);
+ 
+            return binarySearch(arr, mid+1, r, x);
+        }
+        return -1;
+    }
