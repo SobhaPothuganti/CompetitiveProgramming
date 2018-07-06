243b02219e0923b3352b651faacb72e014f4ea6c
SobhaPothuganti <40598670+SobhaPothuganti@users.noreply.github.com>
2018-07-06 11:23:48 +0530

    Update RotationPoint.java

--- week1/day5/RotationPoint.java
+++ week1/day5/RotationPoint.java
@@ -1,1 +1,14 @@
+public class FindRotationPoint {
+    public static int findRotationPoint(String[] words) {
+        int n=words.length-1;
+        int i,l=0,h=n,mid;
+        while(l<h){
+            mid=l+(h-l)/2;
+            if(words[mid].compareTo(words[mid+1]) >1) return mid+1;
+            else if(words[mid-1].compareTo(words[mid]) >1) return mid;
+            else if(words[l].compareTo(words[mid])>1) h=mid-1;
+            else l=mid+1;
+        }
+        return 0;
+    }
 
