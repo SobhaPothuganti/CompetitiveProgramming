eabc505772549ce5902f0bd3992a6b54ea763ba9
SobhaPothuganti <40598670+SobhaPothuganti@users.noreply.github.com>
2018-07-06 11:23:00 +0530

    Update InflightEntertainment.java

--- week1/day5/InflightEntertainment.java
+++ week1/day5/InflightEntertainment.java
@@ -3,7 +3,6 @@ public class InflightEntertainment {
     public static boolean canTwoMoviesFillFlight(int[] movieLengths, int flightLength) {
         int n=movieLengths.length;
         HashMap<Integer,Integer> hm=new HashMap();
-        //int i=0,j=n-1;
         if(n<=1)return false;
         for(int i=0;i<n;i++){
             int k=flightLength-movieLengths[i];
